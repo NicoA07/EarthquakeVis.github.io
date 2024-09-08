5d2051a7437b75d33eb55e4e358642d1bfbdd758
NicoA <90599810+NicoA07@users.noreply.github.com>
2024-09-09 00:32:35 +0800

    Update Map Options

--- group10.js
+++ group10.js
@@ -1,6 +1,6 @@
 // JavaScript code to create the map
 var mapCreated = false;
-var mapCreated2 = false; // ricat
+var mapCreated2 = false;
 
 var map, map2;
 
@@ -210,8 +210,9 @@ function openMap1(evt, mapName) {
             }
         )
         controlLayers.addBaseLayer(light, "Carto Light");
-
-        var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
+        // https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png
+        // https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}
+        var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
             minZoom: 0,
             maxZoom: 20,
             attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
