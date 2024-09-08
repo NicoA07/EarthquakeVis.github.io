162a9a0b2658449503c1870f9a074e0292083818
NicoA <90599810+NicoA07@users.noreply.github.com>
2024-09-09 00:43:56 +0800

    Update Map Options v2

--- group10.js
+++ group10.js
@@ -216,7 +216,8 @@ function openMap1(evt, mapName) {
             minZoom: 0,
             maxZoom: 20,
             attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-            ext: 'png'
+            ext: 'png',
+            apiKey: 'c50f31a2-e162-4ea0-983b-f066af0a5427'
         });
         controlLayers.addBaseLayer(Stadia_AlidadeSmoothDark, "Stadia Black");
 
