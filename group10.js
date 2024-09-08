d5a6a4d38b9863bcf3a48935f89dec449958df80
NicoA <90599810+NicoA07@users.noreply.github.com>
2024-09-09 01:02:53 +0800

    Update Map Options v5

--- group10.js
+++ group10.js
@@ -212,13 +212,12 @@ function openMap1(evt, mapName) {
         controlLayers.addBaseLayer(light, "Carto Light");
         // https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png
         // https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}
-        var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png?api_key={apiKey}', {
+        var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
             minZoom: 0,
             maxZoom: 20,
             // attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
             attribution: '&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>',
-            ext: 'png',
-            apiKey: 'c50f31a2-e162-4ea0-983b-f066af0a5427'
+            ext: 'png'
         });
         controlLayers.addBaseLayer(Stadia_AlidadeSmoothDark, "Stadia Black");
 
